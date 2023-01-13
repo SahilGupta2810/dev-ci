0cc54008e8b8e1d41adf2e652e5ff7f3637777bf
Sahil Gupta <guptasahil2810@gmail.com>
2023-01-13 11:43:44 +0530

    "initial"

--- lib/dev-ci-stack.ts
+++ lib/dev-ci-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as sqs from 'aws-cdk-lib/aws-sqs'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DevCiStack extends cdk.Stack {
@@ -9,8 +10,8 @@ export class DevCiStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'DevCiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new sqs.Queue(this, 'DevCiQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
   }
 }
